6f6cc9e1eb7d42f50fc0eb9110894203f9685ac6
hjpark625 <zero950@gmail.com>
2024-04-16 11:37:03 +0900

    feat: modify webpack build production env

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,55 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html"),
-      filename: "index.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
-            ],
-          },
-        },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".json", ".jsx"],
-    alias: {
-      "@": path.resolve(__dirname, "/src"),
+module.exports = (env, args) => {
+  return {
+    mode: args.mode,
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.[contenthash].js',
+      path: path.resolve(__dirname, 'dist')
     },
-  },
-  devServer: {
-    port: 3001,
-    hot: true,
-    open: false,
-  },
-};
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        filename: 'index.html'
+      })
+    ],
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+            }
+          },
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.json', '.jsx'],
+      alias: {
+        '@': path.resolve(__dirname, '/src')
+      }
+    },
+    devServer: {
+      port: 3001,
+      hot: true,
+      open: false
+    }
+  }
+}
