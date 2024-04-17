1a8be7779fd23dfe1f4d9e731c5fc24d20bca201
hjpark625 <zero950@gmail.com>
2024-04-17 18:08:31 +0900

    feat: postcss-loader 설정추가

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = (env, args) => {
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader']
+          use: ['style-loader', 'css-loader', 'postcss-loader']
         }
       ]
     },
