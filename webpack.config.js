6a3667b974e98f5bb7e4446d833fc30c0fd79a5b
hjpark625 <zero950@gmail.com>
2024-04-17 17:32:55 +0900

    refactor: webpack config code splitting 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, args) => {
     mode: args.mode,
     entry: './src/index.tsx',
     output: {
-      filename: 'bundle.[contenthash].js',
+      filename: '[name].[contenthash].js',
       path: path.resolve(__dirname, 'dist')
     },
     plugins: [
@@ -20,7 +20,26 @@ module.exports = (env, args) => {
     ],
     optimization: {
       minimize: true,
-      minimizer: [new TerserPlugin()]
+      minimizer: [new TerserPlugin()],
+      splitChunks: {
+        chunks: 'all', // 모든 종류의 청크에 대해 코드 스플릿팅 적용
+        minSize: 20000, // 최소 20KB가 넘는 모듈만 분리
+        minChunks: 1, // 모듈이 최소 1개의 청크에서 사용될 때 분리
+        maxAsyncRequests: 30, // 비동기 요청 청크 최대 수
+        maxInitialRequests: 30, // 초기 로딩 청크 최대 수
+        automaticNameDelimiter: '~', // 이름 구분자
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/\]node_modules[\\/]/,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true // 이미 분리된 청크 재사용 여부
+          }
+        }
+      }
     },
     module: {
       rules: [
@@ -53,7 +72,8 @@ module.exports = (env, args) => {
     devServer: {
       port: 3001,
       hot: true,
-      open: false
+      open: false,
+      historyApiFallback: true // SPA 라우팅 지원을 위한 설정
     }
   }
 }
