4318f3e43229771fa6c160758ddfb608902b8ae1
hjpark625 <zero950@gmail.com>
2024-04-23 16:09:00 +0900

    feat: saga 등록

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,25 +1,38 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { useDispatch } from 'react-redux'
-import logger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga'
+import { all } from 'redux-saga/effects'
+import { createLogger } from 'redux-logger'
 
-import counterReducer from '@/store/counter'
+import counterReducer, { counterSaga } from '@/store/counter'
 
 const env = process.env.NODE_ENV
+
+const logger = createLogger()
+const sagaMiddleWare = createSagaMiddleware()
 
 const rootReducer = combineReducers({
   counter: counterReducer
 })
 
+function* rootSaga() {
+  yield all([counterSaga()])
+}
+
 const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => {
     const middlewares = []
+    middlewares.push(sagaMiddleWare)
     if (env !== 'production') {
       middlewares.push(logger)
     }
     return getDefaultMiddleware().concat(middlewares)
   }
 })
+
+sagaMiddleWare.run(rootSaga)
+
 type AppDispatch = typeof store.dispatch
 
 export type RootState = ReturnType<typeof rootReducer>
