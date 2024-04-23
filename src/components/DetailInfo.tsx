352aa96a13ebd9c14807fe98d426d369583a0944
hjpark625 <zero950@gmail.com>
2024-04-23 18:22:44 +0900

    feat: storeDetailPage 제작

---
+++ src/components/DetailInfo.tsx
@@ -0,0 +1,67 @@
+import type { Store } from '@/store/detail/types/storeDetail'
+import { Link } from 'react-router-dom'
+
+interface DetailInfoProps {
+  detail: Store
+}
+
+function DetailInfo({ detail }: DetailInfoProps) {
+  return (
+    <>
+      <div>storeId: {detail.storeId}</div>
+      <div>name: {detail.name}</div>
+      <div>engName: {detail.engName}</div>
+      <div>
+        socialLink: <Link to={detail.socialLink || ''}>{detail.socialLink}</Link>
+      </div>
+      <div>category: {detail.category}</div>
+      <div>address: {detail.address}</div>
+      <div>detailAddress: {detail.detailAddress}</div>
+      <div>phoneNumber: {detail.phoneNumber}</div>
+      <div>
+        operationTime:{' '}
+        {detail.operationTime.map(({ day, startTime, endTime }) => (
+          <div key={day}>
+            <div>day: {day}</div>
+            {startTime && endTime ? (
+              <>
+                <div>startTime: {startTime}</div>
+                <div>endTime: {endTime}</div>
+              </>
+            ) : (
+              '휴무'
+            )}
+          </div>
+        ))}
+      </div>
+      <div>coord: {`{ lat: ${detail.coord.lat}, lng: ${detail.coord.lng} }`}</div>
+      <div>introduce: {detail.introduce ?? '없음'}</div>
+      <div>updatedAt: {`${new Date(detail.updatedAt ?? '').toLocaleDateString()}`}</div>
+      <div>
+        concept:{' '}
+        {detail.concept.length ? detail.concept.map((concept) => <span key={concept}>{concept}</span>) : '없음'}
+      </div>
+      <div>
+        nearestRoute:{' '}
+        {detail.nearestRoute.subwayLine
+          ? detail.nearestRoute.subwayLine.map((subwayLine) => <span key={subwayLine}>{subwayLine}</span>)
+          : '없음'}
+        , {detail.nearestRoute.routeInfo ?? '없음'}
+      </div>
+      <div>
+        storeImages:{' '}
+        {detail.storeImages.length ? (
+          <div className="flex w-screen overflow-x-auto gap-[4px]">
+            {detail.storeImages.map((photo) => (
+              <img key={photo.photoId} src={photo.file_path} alt={photo.photoId} width={300} height={300} />
+            ))}
+          </div>
+        ) : (
+          '이미지 없음'
+        )}
+      </div>
+    </>
+  )
+}
+
+export default DetailInfo
