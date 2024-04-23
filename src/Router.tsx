9dc4d9c5bd1975dc399ec2c5c3bab353bd262493
hjpark625 <zero950@gmail.com>
2024-04-23 15:20:28 +0900

    feat: store provider 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,7 @@
 import { lazy } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from '@/store'
 import '@/styles/main.css'
 
 const CounterPage = lazy(() => import(/* webpackChunkName: "counter_page" */ '@/pages/CounterPage'))
@@ -17,7 +19,11 @@ const routes = createBrowserRouter([
 ])
 
 function Router() {
-  return <RouterProvider router={routes} />
+  return (
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
+  )
 }
 
 export default Router
